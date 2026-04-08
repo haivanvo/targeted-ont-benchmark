e479f77df1ca45d3d06d94f84c028098ceb8c6e2
Hai Van Vo <haivanvowork@gmail.com>
2026-04-08 12:16:40 +0700

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -1,3 +1,16 @@
+export PATH=$PATH:/Users/haivanvo/sratoolkit.3.2.1-mac-x86_64/bin
+which fasterq-dump # Make sure it's correct before converting into fastq file
+prefetch ERR8578835
+fasterq-dump --outdir ~/data_thesis 
+
+# Run NanoPlot to check status
+nanoplot --fastq ~/sra/HG001.fastq \
+  --title "HG001_QC" \
+  --outdir ~/sra/nanoplot_out \
+  --tsv_stats \
+  --N50 \
+-f # choose png/pdf/webpage/jpg...
+
 # Installation
 conda activate thesis
 conda install -c bioconda -c conda-forge nanostat chopper -y

96f8d6ad929dde8ee27eaf498301da9b83fca5d5
Hai Van Vo <haivanvowork@gmail.com>
2026-04-15 11:28:58 +0700

    Update clair3_gpu.sh

--- scripts/clair3_gpu.sh
+++ scripts/clair3_gpu.sh
@@ -1,11 +1,16 @@
 export PATH="/opt/homebrew/opt/gnu-getopt/bin:$PATH"
 
-python3 run_clair3.py \
-  --bam_fn=/Users/haivanvo/HG001.bam \
-  --ref_fn=/Users/haivanvo/data_thesis/GRCh38_no_alt_analysis_set.fasta \
+SAMPLE=/path/to/user
+REF=/path/to/ref/fasta
+MODEL=/path/to/model
+OUTDIR=/path/to/output
+
+python3 /path/to/clair3/run_clair3.py \
+  --bam_fn=${SAMPLE}/HG001.bam \
+  --ref_fn=${REF}/GRCh38_no_alt_analysis_set.fasta \
   --sample_name=HG001 \
   --threads=4 \
   --platform=ont \
-  --model_path=/Users/haivanvo/mamba/envs/clair3/bin/models/r941_e82_400bps_sup_v500 \
-  --output=/Users/haivanvo/tools/Clair3/HG001 \
-  --qual=10
+  --model_path=${MODEL} \
+  --output=${OUTDIR} \
+  --qual=10 # optional 
